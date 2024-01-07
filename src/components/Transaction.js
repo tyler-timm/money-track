68c5e2005cb24823d54d71fd28cd50acb08630b0
Tyler Timm <ttimm16@gmail.com>
2024-01-07 16:22:34 -0600

    Use table rows for transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,10 @@
-const Transaction = ({ transaction, onDelete }) => {
-  return (
-    <div>
-      <h3>
-        {transaction.text} <p onClick = {() => onDelete(transaction.id)}>X </p>
-      </h3>
-      <p>{transaction.category}</p>
-    </div>
-  );
-};
-
-export default Transaction;
+export default function Transaction ({ transaction, onDelete }) {
+    return (
+        <tr>
+            <td className='p-2 border text-right'>{transaction.amount}</td>
+            <td className='p-2 border'>{transaction.text}</td>
+            <td className='p-2 border'>{transaction.category}</td>
+            <td className='p-2 border text-center' onClick={() => onDelete(transaction.id)}>X</td>
+        </tr>
+    );
+}
