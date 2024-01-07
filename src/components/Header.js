d0bb69465366f6d520e60f14eb31e05cb072f75d
Tyler Timm <ttimm16@gmail.com>
2024-01-07 16:40:33 -0600

    Update title in metadata, header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
-const Header = ({ title }) => {
+export default function Header() {
     return (
         <div>
-            <h1 className='text-4xl mb-5'>{title}</h1>
+            <h1 className='text-4xl mb-5'>Money Track</h1>
         </div>
     );
-};
-
-export default Header;
+}
