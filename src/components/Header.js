5179d0ac7d08ae8f0cb119eefda6a723bc07f2cc
Tyler Timm <ttimm16@gmail.com>
2024-01-10 00:00:27 -0600

    Update title style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div>
-            <h1 className='text-4xl mb-5'>Money Track</h1>
+            <h1 className='text-4xl text-bold mb-5 text-yellow-500'>Money Track</h1>
         </div>
     );
 }
