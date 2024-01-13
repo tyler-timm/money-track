2ec6bf7e89aab8e1b4d501c08d5c173af14daa44
Tyler Timm <ttimm16@gmail.com>
2024-01-13 14:09:05 -0600

    Add inputs to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,18 @@
-export default function Modal({ showModal }) {
-    if (!showModal) return null;
+export default function Modal({ isModalVisible }) {
+    if (!isModalVisible) return null;
     return (
-        <>
-            This is a Modal
-        </>
-    );
-};
+        <div className='border mb-5'>
+            Add Transaction
+            <br/>
+            <label for='amount'>Amount:</label>
+            <input id='amount' className='border m-2'></input>
+            <br/>
+            <label for='type'>Type:</label>
+            <input id='type' className='border m-2'></input>
+            <br/>
+            <label for='category'>Category: </label>
+            <input id='category' className='border m-2'></input>
+            <br/>
+        </div>
+    )
+}
