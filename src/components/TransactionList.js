0cf6c1166480d40b053485674106993c65ccc81a
Tyler Timm <ttimm16@gmail.com>
2024-01-16 23:56:56 -0600

    Add tbody tag to remove warning message

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,26 +1,28 @@
 import Transaction from "./Transaction";
 
-export default function TransactionList ({ transactions, onDelete }) {
+export default function TransactionList({ transactions, onDelete }) {
     let total = 0;
     transactions.forEach(tran => total += tran.amount);
 
     return (
         <table>
-            <tr>
-                <td className='p-2 border text-bold'>Amount</td>
-                <td className='p-2 border text-bold'>Description</td>
-                <td className='p-2 border text-bold'>Type</td>
-                <td className='p-2 border text-bold'>Delete</td>
-            </tr>
+            <tbody>
+                <tr>
+                    <td className='p-2 border text-bold'>Amount</td>
+                    <td className='p-2 border text-bold'>Description</td>
+                    <td className='p-2 border text-bold'>Type</td>
+                    <td className='p-2 border text-bold'>Delete</td>
+                </tr>
 
-            {transactions.map((transaction, id) => (
-                <Transaction key={id} transaction={transaction} onDelete={onDelete} />
-            ))}
-            
-            <tr>
-                <td className='p-2 border text-right'>{total}</td>
-                <td className='p-2 border' colspan='3'>Total</td>
-            </tr>
+                {transactions.map((transaction, id) => (
+                    <Transaction key={id} transaction={transaction} onDelete={onDelete} />
+                ))}
+
+                <tr>
+                    <td className='p-2 border text-right'>{total}</td>
+                    <td className='p-2 border' colSpan='3'>Total</td>
+                </tr>
+            </tbody>
         </table>
     );
 }
