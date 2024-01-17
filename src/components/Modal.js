a64ec245e010c11a4d1143e6703c4985cd0e1cd1
Tyler Timm <ttimm16@gmail.com>
2024-01-16 23:56:38 -0600

    Make form a 'controlled' form - data being submitted is logged in console - Minor spacing changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,61 @@
+import { useState } from "react";
 import Button from '../components/Button';
 
 export default function Modal({ isModalVisible, hideOnCancel }) {
+    const [amount, setAmount] = useState('');
+    const [type, setType] = useState('');
+    const [category, setCategory] = useState('');
+
+    const submitData = () => {
+        console.log({
+            amount: amount,
+            type: type,
+            category: category
+        });
+    }
+
     if (!isModalVisible) return null;
     return (
-        <div className='border mb-5'>
+        <div className='border mb-5 p-3'>
             Add Transaction
             <br />
-            <label for='amount'>Amount:</label>
-            <input id='amount' className='border m-2'></input>
+            <label>
+                Amount:
+                <input
+                    value={amount}
+                    onChange={e => setAmount(e.target.value)}
+                    type='text'
+                    className='border m-2 text-black'
+                />
+            </label>
             <br />
-            <label for='type'>Type:</label>
-            <input id='type' className='border m-2'></input>
+            <label>
+                Type:
+                <input
+                    value={type}
+                    onChange={e => setType(e.target.value)}
+                    type='text'
+                    className='border m-2 text-black'
+                />
+            </label>
             <br />
-            <label for='category'>Category: </label>
-            <input id='category' className='border m-2'></input>
+            <label>
+                Category:
+                <input
+                    value={category}
+                    onChange={e => setCategory(e.target.value)}
+                    type='text'
+                    className='border m-2 text-black'
+                />
+            </label>
             <br />
             <Button
-                className='bg-blue-500 hover:bg-blue-600 drop-shadow-sm px-2 py-1 rounded'
+                className='bg-green-600 hover:bg-green-700 drop-shadow-sm px-2 py-1 mr-4 rounded'
+                onClick={submitData}
+                text='Submit'
+            />
+            <Button
+                className='bg-blue-600 hover:bg-blue-700 drop-shadow-sm px-2 py-1 rounded'
                 onClick={hideOnCancel}
                 text='Cancel'
             />
