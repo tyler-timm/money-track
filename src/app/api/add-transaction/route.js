01cbe6f286803c71bcfde67efd086ea270f8bc87
Tyler Timm <ttimm16@gmail.com>
2024-01-20 23:30:50 -0600

    Add API Route to create transaction

---
+++ src/app/api/add-transaction/route.js
@@ -0,0 +1,19 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const amount = searchParams.get('amount');
+    const description = searchParams.get('description');
+    const type = searchParams.get('type');
+
+    try {
+        if (!amount || !description || !type) throw new Error('All fields required');
+        await sql`INSERT INTO Transactions (Amount, Description, Type) VALUES (${amount}, ${desctiption}, ${type});`;
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+
+    const transactions = await sql`SELECT * FROM Transactions;`;
+    return NextResponse.json({ transactions }, { status: 200 });
+}
