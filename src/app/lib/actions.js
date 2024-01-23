a1dcea1f7f6bc8e71fc9e42938b6cc5f8f701593
Tyler Timm <ttimm16@gmail.com>
2024-01-22 22:06:33 -0600

    Remove 'controlled' inputs to use server action

---
+++ src/app/lib/actions.js
@@ -0,0 +1,7 @@
+'use server';
+
+export async function createTransaction(formData: formData) { 
+    const formData = {
+        
+    }
+}
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import Button from '../components/Button';
+import { useState } from 'react';
+import { createTransaction } from '@/app/lib/actions';
 
 export default function Modal({ isModalVisible, hideOnCancel }) {
-    const [amount, setAmount] = useState('');
-    const [type, setType] = useState('');
-    const [category, setCategory] = useState('');
+    // const [amount, setAmount] = useState('');
+    // const [type, setType] = useState('');
+    // const [category, setCategory] = useState('');
 
     const submitData = () => {
         console.log({
@@ -16,49 +17,57 @@ export default function Modal({ isModalVisible, hideOnCancel }) {
 
     if (!isModalVisible) return null;
     return (
-        <div className='border mb-5 p-3'>
-            Add Transaction
-            <br />
-            <label>
-                Amount:
-                <input
-                    value={amount}
-                    onChange={e => setAmount(e.target.value)}
-                    type='text'
-                    className='border m-2 text-black'
+        <form action={createTransaction}>
+            <div className='border mb-5 p-3'>
+                Add Transaction
+                <br />
+                <label>
+                    Amount:
+                    <input
+                        id='amount'
+                        name='amount'
+                        type='text'
+                        className='border m-2 text-black'
+                    />
+                </label>
+                <br />
+                <label>
+                    Type:
+                    <input
+                        id='type'
+                        name='type'
+                        type='text'
+                        className='border m-2 text-black'
+                    />
+                </label>
+                <br />
+                <label>
+                    Description:
+                    <input
+                        id='description'
+                        name='description'
+                        type='text'
+                        className='border m-2 text-black'
+                    />
+                </label>
+                <br />
+                <Button
+                    className='bg-green-600 hover:bg-green-700 drop-shadow-sm px-2 py-1 mr-4 rounded'
+                    onClick={submitData}
+                    text='Submit'
                 />
-            </label>
-            <br />
-            <label>
-                Type:
-                <input
-                    value={type}
-                    onChange={e => setType(e.target.value)}
-                    type='text'
-                    className='border m-2 text-black'
+                <Button
+                    className='bg-blue-600 hover:bg-blue-700 drop-shadow-sm px-2 py-1 rounded'
+                    onClick={hideOnCancel}
+                    text='Cancel'
                 />
-            </label>
-            <br />
-            <label>
-                Category:
-                <input
-                    value={category}
-                    onChange={e => setCategory(e.target.value)}
-                    type='text'
-                    className='border m-2 text-black'
-                />
-            </label>
-            <br />
-            <Button
-                className='bg-green-600 hover:bg-green-700 drop-shadow-sm px-2 py-1 mr-4 rounded'
-                onClick={submitData}
-                text='Submit'
-            />
-            <Button
-                className='bg-blue-600 hover:bg-blue-700 drop-shadow-sm px-2 py-1 rounded'
-                onClick={hideOnCancel}
-                text='Cancel'
-            />
-        </div>
+            </div>
+        </form>
     )
 }
+
+/* 
+Good to know: In HTML, you'd pass a URL to the action attribute. This URL would be the destination where your form data should be submitted (usually an API endpoint).
+However, in React, the action attribute is considered a special prop - meaning React builds on top of it to allow actions to be invoked.
+Behind the scenes, Server Actions create a POST API endpoint. This is why you don't need to create API endpoints manually when using Server Actions.
+ */
