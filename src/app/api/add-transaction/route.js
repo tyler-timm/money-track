7a84dd887a32a010934565096afbd3cdaed0d814
Tyler Timm <ttimm16@gmail.com>
2024-02-04 16:20:30 -0600

    Revert "Update schema, attempt to update, read db"
    This reverts commit 27b1bd371fc090c14135aec47ff0f484a41691bb.

--- src/app/api/add-transaction/route.js
+++ src/app/api/add-transaction/route.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
-
-const prisma = new PrismaClient();
 
 export async function GET(request) {
     const { searchParams } = new URL(request.url);
@@ -11,16 +9,7 @@ export async function GET(request) {
 
     try {
         if (!amount || !description || !type) throw new Error('All fields required');
-        // const amountCents = (amount.includes('.') ? amount * 100 : amount);
-        const newTransaction = await prisma.transaction.create({
-            data: {
-                amount: amount,
-                description: description,
-                type: type
-            }
-        });
-
-        // await sql`INSERT INTO Transactions (Amount, Description, Type) VALUES (${amount}, ${description}, ${type});`;
+        await sql`INSERT INTO Transactions (Amount, Description, Type) VALUES (${amount}, ${description}, ${type});`;
     } catch (error) {
         if( error instanceof Prisma.PrismaClientValidationError){
         return NextResponse.json({ error.message }, { status: 500 });
@@ -28,8 +17,6 @@ export async function GET(request) {
         return NextResponse.json({ error }, { status: 500 });
     }
 
-    // Return transactions in database
-    const transactions = await prisma.transaction.findMany();
-    // const transactions = await sql`SELECT * FROM Transactions;`;
+    const transactions = await sql`SELECT * FROM Transactions;`;
     return NextResponse.json({ transactions }, { status: 200 });
 }
