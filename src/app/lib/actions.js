628684fc57b356291a68e8bdd38fc70d5dad75fa
Tyler Timm <ttimm16@gmail.com>
2024-02-04 16:19:09 -0600

    Revert the reverted commit

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,7 +1,11 @@
 'use server';
 
-export async function createTransaction(formData: formData) { 
-    const formData = {
-        
+export async function createTransaction(formData) { 
+    const rawFormData = {
+        amount: formData.get('amount'),
+        type: formData.get('type'),
+        description: formData.get('description')
     }
+
+    console.log(`rawFormData: ${JSON.stringify(rawFormData)}`);
 }
