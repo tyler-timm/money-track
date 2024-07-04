5e0096d3336815f9d62e55e108522876cb115d56
Tyler Timm <ttimm16@gmail.com>
2024-07-03 21:13:12 -0500

    Create flex container in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <div>
-            <h1 className='text-5xl text-bold mb-5 text-yellow-500'>Money Track</h1>
+        <div className='flex flex-col items-center justify-center'>
+            <h1 className='text-5xl text-bold p-5 text-yellow-500'>Money Track</h1>
         </div>
     );
 }
