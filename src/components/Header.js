ad4b48579a965ec4ff152138c2f6462e99d58371
Tyler Timm <ttimm16@gmail.com>
2024-08-04 22:40:11 -0500

    Add favicon and flying piggy bank icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from '@clerk/nextjs';
 import { SignInButton } from '@clerk/nextjs';
 import { UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Header() {
     return (
@@ -8,18 +9,26 @@ export default function Header() {
             className='flex flex-col justify-center items-center top-0 fixed w-full z-20'
             style={{ backgroundColor: "#264653" }}
         >
-            <h1 className='text-5xl font-bold p-5 text-yellow-500'>Money Track</h1>
-            <SignedOut>
-                <ul className='self-end justify-center mr-10 text-yellow-500 text-xl'>
-                    <SignInButton />
-                </ul>
-                <h2 className='text-lg'>Please sign in to view your transactions</h2>
-            </SignedOut>
-            <SignedIn>
-                <ul className='self-end justify-center mr-10 text-yellow-500 text-xl'>
-                    <UserButton />
-                </ul>
-            </SignedIn>
+            <div className='flex'>
+                <Image src='/flying-piggy-bank.png'
+                    width='90'
+                    height='90'
+                    alt='Flying Piggy Bank'
+                    className='inline'
+                />
+                <h1 className='text-5xl font-bold p-5 text-yellow-500'>Money Track</h1>
+            </div>
+                <SignedOut>
+                    <ul className='self-end justify-center mr-10 text-yellow-500 text-xl'>
+                        <SignInButton />
+                    </ul>
+                    <h2 className='text-lg'>Please sign in to view your transactions</h2>
+                </SignedOut>
+                <SignedIn>
+                    <ul className='self-end justify-center mr-10 text-yellow-500 text-xl'>
+                        <UserButton />
+                    </ul>
+                </SignedIn>
         </div >
     );
 }
